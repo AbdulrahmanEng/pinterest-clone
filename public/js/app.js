52ebef665a4abfdf5d12dc9c2a791609102f9dcd
Abdulrahman I <bydeinon@gmail.com>
2019-04-09 05:02:06 +0000

    Fix the grid

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,8 @@
-// Get the grid element.
-var elem = document.querySelector('.grid');
-
-// If they are all true call the Masonry function.
-var msnry = new Masonry(elem, {
-    itemSelector: '.grid-item',
-    columnWidth: 100,
-    fitWidth: true
-});
+window.onload = function() {
+    // If they are all true call the Masonry function.
+    var msnry = new Masonry('.masonry', {
+        itemSelector: '.grid-item',
+        columnWidth: 100,
+        fitWidth: true
+    });
+}
